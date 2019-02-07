0b0827b7bd218059bdb18df29acd10066ef1e109
yhctung <yhc.tung@gmail.com>
2019-02-07 15:00:16 -0600

    Added character count

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,10 +3,13 @@ package com.codepath.apps.restclienttemplate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -24,6 +27,7 @@ public class ComposeActivity extends AppCompatActivity {
 
     private EditText etCompose;
     private Button btnTweet;
+    private TextView tvCount;
     private TwitterClient client;
 
     @Override
@@ -34,6 +38,7 @@ public class ComposeActivity extends AppCompatActivity {
         client = TwitterApp.getRestClient(this);
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+        tvCount = findViewById(R.id.tvCount);
 
         // Set click listener on button
         btnTweet.setOnClickListener(new View.OnClickListener(){
@@ -50,8 +55,9 @@ public class ComposeActivity extends AppCompatActivity {
                     return;
                 }
                 // for testing
-                Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
+                // Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
                 // Make API call to Twitter to publish the content in edit text
+
                 client.composeTweet(tweetContent, new JsonHttpResponseHandler(){
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
@@ -73,6 +79,27 @@ public class ComposeActivity extends AppCompatActivity {
                     }
                 });
             }
+        });
+
+
+
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Fires right before text is changing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Fires right after the text has changed
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Fires right as the text is being changed (even supplies the range of text)
+                tvCount.setText(Integer.toString(140 - etCompose.length()));
+            }
+
         });
 
     }
